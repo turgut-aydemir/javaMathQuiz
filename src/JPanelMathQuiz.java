2c537a484637eb8c1d4c706fef2f06f082a1fe37
Turgut Aydemir <turgut.aydemir@compax.at>
2023-02-23 15:23:48 +0100

    Model View Controller added.

--- src/JPanelMathQuiz.java
+++ src/JPanelMathQuiz.java
@@ -4,29 +4,33 @@ import javax.swing.*;
 
 public class JPanelMathQuiz extends JFrame implements ActionListener { // Main class extending JFrame class
     static JFrame f; // JFrame
-    static JButton b1, b2, b3, b4; // JButton
+    static JButton buttonAddQuestion, buttonEditQuestions, buttonStartQuiz, buttonHighScore, buttonBack, buttonStart; // JButton
+    //public JPanel panelMainMenu;
 
     JPanelMathQuiz(String s)
     {
         f = new JFrame("Math Quiz"); // Creating a new frame to store text field and button
 
-        b1 = new JButton("Add Question"); // Creating a new buttons
-        b1.addActionListener(this);
-        b2 = new JButton("Edit Questions");
-        b2.addActionListener(this);
-        b3 = new JButton("Start Quiz");
-        b3.addActionListener(this);
-        b4 = new JButton("High Score");
-        b4.addActionListener(this);
-
+        buttonAddQuestion = new JButton("Add Question"); // Creating a new buttons
+        buttonAddQuestion.addActionListener(this);
+        buttonEditQuestions = new JButton("Edit Questions");
+        buttonEditQuestions.addActionListener(this);
+        buttonStartQuiz = new JButton("Start Quiz");
+        buttonStartQuiz.addActionListener(this);
+        buttonHighScore = new JButton("High Score");
+        buttonHighScore.addActionListener(this);
+        buttonBack = new JButton("Back");
+        buttonBack.addActionListener(this);
+        buttonStart = new JButton("Start");
+        buttonStart.addActionListener(this);
         JPanel panelMainMenu = new JPanel(); // Creating a panel to add buttons and textfield and a layout
 
         //panelMainMenu.setLayout(new BoxLayout(panelMainMenu, BoxLayout.Y_AXIS)); // Setting box layout
 
-        panelMainMenu.add(b1); // Adding buttons and textfield to panel
-        panelMainMenu.add(b2);
-        panelMainMenu.add(b3);
-        panelMainMenu.add(b4);
+        panelMainMenu.add(buttonAddQuestion); // Adding buttons and textfield to panel
+        panelMainMenu.add(buttonEditQuestions);
+        panelMainMenu.add(buttonStartQuiz);
+        panelMainMenu.add(buttonHighScore);
 
         panelMainMenu.setBackground(Color.green); // Setting background of panel
         panelMainMenu.setBorder(BorderFactory.createEmptyBorder(40,40,40,40));
@@ -41,45 +45,53 @@ public class JPanelMathQuiz extends JFrame implements ActionListener { // Main c
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==b1)
+        if(e.getSource()== buttonAddQuestion)
         {
-            b1.setEnabled(false);
-            b1.setText("you can add questions");
+            buttonAddQuestion.setEnabled(false);
+            buttonAddQuestion.setText("you can add questions");
         }
-        if(e.getSource()==b2)
+        if(e.getSource()== buttonEditQuestions)
         {
-            b2.setText("editing questions");
+            buttonEditQuestions.setText("editing questions");
             setVisible(false);
             setVisible(true);
         }
-        if(e.getSource()==b3)
+        if(e.getSource()== buttonStartQuiz)
             {
                 JPanel panelStartQuiz = new JPanel();
-                JLabel usernameLabel = new JLabel("Name");
-                JLabel numberOFQuestionsLabel = new JLabel("# of Questions");
-                JTextField usernameTextField = new JTextField("0",10);
-                JTextField numberOFQuestionsTextField = new JTextField("0",10);
+                JLabel usernameLabel = new JLabel("Name: ");
+                JLabel numberOFQuestionsLabel = new JLabel("Number of questions: ");
+                JTextField usernameTextField = new JTextField("",1);
+                JTextField numberOFQuestionsTextField = new JTextField("",1);
                 panelStartQuiz.add(usernameLabel);
                 panelStartQuiz.add(usernameTextField);
                 panelStartQuiz.add(numberOFQuestionsLabel);
                 panelStartQuiz.add(numberOFQuestionsTextField);
-                b1 = new JButton("Back"); // Creating a new buttons
-                b1.addActionListener(this);
-                b2 = new JButton("Start");
-                b2.addActionListener(this);
-                panelStartQuiz.add(b1); // Adding buttons and textfield to panel
-                panelStartQuiz.add(b2);
+                JButton bBack = new JButton("Back"); // Creating a new buttons
+                bBack.addActionListener(this);
+                JButton bStart = new JButton("Start");
+                bStart.addActionListener(this);
+                panelStartQuiz.add(bBack); // Adding buttons and textfield to panel
+                panelStartQuiz.add(bStart);
                 panelStartQuiz.setBackground(Color.yellow); // Setting background of panel
                 panelStartQuiz.setBorder(BorderFactory.createEmptyBorder(40,40,40,40));
-                panelStartQuiz.setLayout(new GridLayout(4,2));
+                panelStartQuiz.setLayout(new GridLayout(3,2));
                 f.setContentPane(panelStartQuiz);
                 f.invalidate();
                 f.validate();
                 //b3.setText("how many questions do you want AND what is your name?");
                 //Main myMain = new Main();
                 //myMain.randomizer(0);
+                if(e.getSource()==bBack)
+                {
+                    new JPanelMathQuiz("Math Quiz");
+                }
+                if(e.getSource()==bStart)
+                {
+                    new JPanelMathQuiz("Math Quiz");
+                }
         }
-        if(e.getSource()==b4)
+        if(e.getSource()== buttonHighScore)
         {
             JOptionPane.showMessageDialog(this,"High Score");
             System.exit(0);
